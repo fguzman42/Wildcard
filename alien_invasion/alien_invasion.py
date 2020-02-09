0ed93c6932c928b6ff25e0649aa9434d0fbbe14f
fguzman42 <frisco530@gmail.com>
2020-02-09 09:24:31 -0800

    checkout out

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 		def __init__(self):
@@ -13,12 +14,18 @@ class AlienInvasion:
 			self.settings.screen_height = self.screen.get_rect().height
 			pygame.display.set_caption("test :)")
 			self.ship = Ship(self)
+			self.bullets = pygame.sprite.Group()
 		
 		def run_game(self):
 			while True:
 				self._check_events()
 				self.ship.update()
+				self._update_bullets()
 				self._update_screen()
+				for bullet in self.bullets.copy():
+					if bullet.rect.bottom <= 0:
+    						self.bullets.remove(bullet)
+				print(len(self.bullets))
 
 		def _check_events(self):
 			for event in pygame.event.get():
@@ -36,6 +43,13 @@ class AlienInvasion:
 					self.ship.moving_left = True
 				elif event.key == pygame.K_ESCAPE:
 					sys.exit()
+				elif event.key == pygame.K_SPACE:
+					self._fire_bullet()
+
+		def _fire_bullet(self):
+			if len(self.bullets) < self.settings.bullets_allowed:
+				new_bullet = Bullet(self)
+				self.bullets.add(new_bullet)
 
 		def	_check_keyup_events(self, event):
 				if event.key == pygame.K_RIGHT:
@@ -43,9 +57,17 @@ class AlienInvasion:
 				elif event.key == pygame.K_LEFT:
 					self.ship.moving_left = False
 
+		def _update_bullets(self):
+			self.bullets.update()
+			for bullet in self.bullets.copy():
+				if bullet.rect.bottom <= 0:
+					self.bullets.remove(bullet)
+
 		def _update_screen(self):
 			self.screen.fill(self.settings.bg_color)
 			self.ship.blitme()
+			for bullet in self.bullets.sprites():
+				bullet.draw_bullet()
 
 			pygame.display.flip()
 
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,8 +4,9 @@ class Settings:
 		self.screen_width = 1200
 		self.screen_height =  1200
 		self.bg_color = (230, 230, 230)
-		self.ship_speed = 5.5
-		self.bullet_speed = 1.0
+		self.ship_speed = 10
+		self.bullet_speed = 15.0
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
+		self.bullets_allowed = 3
