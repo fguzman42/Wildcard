ad462f65cfacea5c072a34268e67dbd1dd45ad21
fguzman42 <frisco530@gmail.com>
2020-02-15 13:02:27 -0800

    game runs well now, ship shoots bullets, aliens move and aliens can get destroyed

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -3,10 +3,14 @@ class Settings:
 	def __init__(self):
 		self.screen_width = 1200
 		self.screen_height =  1200
-		self.bg_color = (230, 230, 230)
-		self.ship_speed = 10
-		self.bullet_speed = 15.0
-		self.bullet_width = 3
-		self.bullet_height = 15
+		self.bg_color = (80, 80, 80)
+		self.ship_speed = 60
+		self.ship_limit = 3
+		self.bullet_speed = 50.0
+		self.bullet_width = 100
+		self.bullet_height = 10
 		self.bullet_color = (60, 60, 60)
-		self.bullets_allowed = 3
+		self.bullets_allowed = 100000
+		self.alien_speed = 60.0
+		self.fleet_drop_speed = 50
+		self.fleet_direction = 1
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,7 +1,6 @@
 import pygame
 
 class Ship:
-
     def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
@@ -19,5 +18,10 @@ class Ship:
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         self.rect.x = self.x
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
